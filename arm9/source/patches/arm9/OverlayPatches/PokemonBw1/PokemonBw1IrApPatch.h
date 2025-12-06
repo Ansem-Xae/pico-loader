005b8cab03204a1699d4e4aa21a8a681ec89f1cc
taxicat1 <32942550+taxicat1@users.noreply.github.com>
2025-12-06 08:51:58 -0500

    Initial generalize Pokemon IR patching

--- arm9/source/patches/arm9/OverlayPatches/PokemonBw1/PokemonBw1IrApPatch.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "../OverlayPatch.h"
-
-/// @brief Arm9 overlay patch to disable the Pokemon Black & White IR-sensor anti-piracy.
-class PokemonBw1IrApPatch : public OverlayPatch
-{
-public:
-    const void* InsertPatch(PatchContext& patchContext) override;
-};
---
+++ common/PokemonIrVersion.h
@@ -0,0 +1,9 @@
+#pragma once
+
+/// @brief Enum representing a Pokemon game version with IR-sensor based anti-piracy
+enum class PokemonIrVersion : u32
+{
+    Hgss,
+    Bw1,
+    Bw2
+};
